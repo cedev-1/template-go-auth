18f61e7b0b06ce57474aa09fe60909684a985c3d
cedev-1 <121963179+cedev-1@users.noreply.github.com>
2026-01-08 19:01:46 +0100

    init nix flake config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Go auth template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            go-task
+            golangci-lint
+            goimports-reviser
+          ];
+        };
+      });
+    };
+}
